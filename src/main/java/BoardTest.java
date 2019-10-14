9ae639248972fdab7910effc4dade0ac2fcc284c
mdejesus <marco_de_jesus@epam.com>
2019-10-13 19:47:53 -0500

    Creating the first test to check 200 response in the boards

--- src/main/java/BoardTest.java
+++ src/main/java/BoardTest.java
@@ -7,5 +7,12 @@ public class BoardTest {
     private static final String AUTH_TOKEN = "";
     private static final String BOARD_IDENTIFIER = "5d9d216eb3311c26a444c92f";
 
-
+    @Test
+    public void ValidateOKResponseByID(){
+        given()
+                .when()
+                .get("https://api.trello.com/1/boards/"+ BOARD_IDENTIFIER +"?key="+ APP_KEY +"&token="+ AUTH_TOKEN)
+                .then()
+                .statusCode(200);
+    }
 }
