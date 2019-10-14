b4e041f73176c917ffb5033c0b5ae53b2b4e56b4
mdejesus <marco_de_jesus@epam.com>
2019-10-13 19:44:11 -0500

    Adding Constants

--- src/main/java/BoardTest.java
+++ src/main/java/BoardTest.java
@@ -2,4 +2,10 @@ import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
 
 public class BoardTest {
+
+    private static final String APP_KEY = "";
+    private static final String AUTH_TOKEN = "";
+    private static final String BOARD_IDENTIFIER = "5d9d216eb3311c26a444c92f";
+
+
 }
