851f43527e8b466f4d4c8b6408d997d092fa256a
mdejesus <marco_de_jesus@epam.com>
2019-10-13 19:52:33 -0500

    Modeling the Response Properties

--- src/main/java/dataModel/Board.java
+++ src/main/java/dataModel/Board.java
@@ -4,4 +4,14 @@ import org.codehaus.jackson.annotate.JsonProperty;
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 
 public class Board {
+    @JsonProperty
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
